8555174174ab2ffb0ab091a59a2804284820ace4
William <nearata@protonmail.com>
2022-09-14 19:05:59 +0200

    fix translations

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,19 +1,23 @@
 import app from "flarum/admin/app";
 import Button from "flarum/common/components/Button";
 
+const trans = (key: string, params = {}) => {
+    return app.translator.trans(
+        `nearata-cloudflare.admin.settings.${key}`,
+        params
+    );
+};
+
 app.initializers.add("nearata-cloudflare", () => {
     app.extensionData
         .for("nearata-cloudflare")
         .registerSetting({
             setting: "nearata-cloudflare.api-key",
             type: "password",
-            label: app.translator.trans(
-                "nearata-cloudflare.admin.settings.api_key"
-            ),
-            help: app.translator.trans(
-                "nearata-cloudflare.admin.settings.api_key_help",
-                { url: "https://developers.cloudflare.com/api/tokens/create" }
-            ),
+            label: trans("api_key"),
+            help: trans("api_key_help", {
+                url: "https://developers.cloudflare.com/api/tokens/create",
+            }),
         })
         .registerSetting(function () {
             return m(".Form-group", [
@@ -47,7 +51,7 @@ app.initializers.add("nearata-cloudflare", () => {
                                 });
                         },
                     },
-                    "Refresh Zone ID"
+                    trans("refresh_zone_button_label")
                 ),
             ]);
         });
